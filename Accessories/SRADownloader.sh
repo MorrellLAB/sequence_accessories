8ec9cefc191ba1f7c86be2cf5dd173a6e7551646
Paul Hoffman <hoff0792@umn.edu>
2016-12-05 09:52:49 -0600

    Moved file

--- Accessories/SRADownloader.sh
+++ Accessories/SRADownloader.sh
@@ -1,13 +1,10 @@
 #!/bin/env sh
 
-#PBS -l mem=12gb,nodes=1:ppn=1,walltime=24:00:00 
-#PBS -m abe 
-#PBS -M pmorrell@umn.edu 
-#PBS -q lab
-
-#    Peter L. Morrell, 18 October 2016, St. Paul, MN
-#    Dependencies: Tom Kono's SRA_Fetch.sh script
-#    Available at: https://github.com/TomJKono/Misc_Utils/blob/master/SRA_Fetch.sh
+#   Written by Peter L. Morrell, 18 October 2016, St. Paul, MN
+#   Adapted by Paul Hoffman
+#   Inspired by Tom Kono's SRA_Fetch.sh script
+#   Original SRA_Fetch.sh available at:
+#   https://github.com/TomJKono/Misc_Utils/blob/master/SRA_Fetch.sh
 
 #    Path to file listing SRA files to download
 SRA_FILES=${HOME}/scratch/IPK_Landraces/IPK_landraces_SRA.txt
@@ -19,16 +16,16 @@ OUTPUT=${HOME}/scratch/IPK_Landraces
 if ! [[ -f "${SRA_FILES}" ]]
     then echo "Failed to find ${SRA_FILES}, exiting..." >&2
     exit 1
-    fi 
+    fi
 
 #   Make the array using command substitution
-declare -a SRA_ARRAY=($(cat "${SRA_FILES}")) 
+declare -a SRA_ARRAY=($(cat "${SRA_FILES}"))
 
 #   Print the values of the array to screen
 printf '%s\n' "${SRA_ARRAY[@]}"
 
 #   location of SRA download script, from Tom Kono's Misc_Utils
-SRA_FETCH=${HOME}/Apps/TKono/Misc_Utils/SRA_Fetch.sh 
+SRA_FETCH=${HOME}/Apps/TKono/Misc_Utils/SRA_Fetch.sh
 
 #   iterate over every each of the run numbers in a lit of SRA files
 #   and download to specified directory
@@ -37,4 +34,18 @@ SRA_FETCH=${HOME}/Apps/TKono/Misc_Utils/SRA_Fetch.sh
 for i in "${SRA_ARRAY[@]}"
     do bash $SRA_FETCH -r $i -d $OUTPUT
     done
+
+declare -a sra_deps=()
+
+function SRAUsage(){
+    echo -e "\
+" >&2
+    exit 1
+}
+
+export -f SRAUsage
+
+
+
+
 
